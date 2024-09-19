7b33e6bc6748f62063ca341fd9da81709bea8d90
Jacob DeWar <whiterbread@gmail.com>
2024-09-19 16:07:27 -0400

    adding type declaration

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwind from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({
+    insertTypesEntry: true,
+  }),],
   build: {
     lib: {
       entry: 'src/index.tsx',
